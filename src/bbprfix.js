9df6dcc77043fb696cb5a000d74e6305b05b7d2f
Yoichi Imai <sunnyone41@gmail.com>
2016-12-07 12:58:53 +0900

    translateProperties

--- src/bbprfix.js
+++ src/bbprfix.js
@@ -1,6 +1,37 @@
-$('.aui-sidebar').width('400px');
-$('.aui-sidebar-wrapper').width('400px');
-$('.aui-page-panel').css('padding-left', '400px');
-$('ul.commit-files-summary').clone().appendTo('.aui-sidebar-body');
-$('ul.commit-files-summary li:odd').css('background-color', '#e6e6e6');
-$('.aui-sidebar .commit-files-summary--filename').each(function() { $(this).text($(this).text().replace(/src\/.*?\/java\/com\/.*?\//, '.../')) });
+
+function replaceSidebar() {
+  $('.aui-sidebar').width('400px');
+  $('.aui-sidebar-wrapper').width('400px');
+  $('.aui-page-panel').css('padding-left', '400px');
+  $('ul.commit-files-summary').clone().appendTo('.aui-sidebar-body');
+  $('ul.commit-files-summary li:odd').css('background-color', '#e6e6e6');
+  $('.aui-sidebar .commit-files-summary--filename').each(function() { $(this).text($(this).text().replace(/src\/.*?\/java\/com\/.*?\//, '.../')) });
+}
+
+function translateProperties() {
+  var diffs = [];
+
+  $("h1.filename").each(function() {
+    var elem = this;
+    if (elem.innerText.search(/\.properties/) < 0) {
+      return;
+    }
+    var container = $(elem).parents(".diff-container");
+    diffs.push(container);
+  });
+
+  diffs.forEach(function (diff) {
+    $(diff).find("pre.source").each (function() {
+      var elem = this;
+      var origText = elem.textContent;
+      var newText = origText.replace(/\\u([0-9A-Z]{4})/g, function (match, p1) {
+        var c = Number.parseInt(p1, 16);
+        return String.fromCodePoint(c);
+      });
+      elem.textContent = newText;
+    });
+  });
+}
+
+translateProperties();
+replaceSidebar();
